a771b2a5629b1085a8a4b95b1e657731a0f2c8db
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-04-19 18:11:15 +0530

    Main file

---
+++ CalculatorMain.java
@@ -0,0 +1,13 @@
+// CalculatorMain.java
+// Name: Neil Cardoz
+// PRN:  23070126079    
+// Batch: 23-27
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        UserInput ui = new UserInput();
+        ui.displayMenu();
+    }
+}
+
+// This is the main file of the calculator 
